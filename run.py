7e51d653fbd9d2bddf7bcc4761ea8f2cc8e0724d
UMTti <toivanenpihla@gmail.com>
2016-05-28 15:31:39 +0300

    kaikki

--- run.py
+++ run.py
@@ -3,6 +3,11 @@ from flask import Flask, url_for
 from flask import render_template
 app = Flask(__name__)
 
+@app.route('/send_beacon_id/<String:beacon_id>')
+def send_beacon_id(beacon_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % beacon_id
+
 
 @app.route('/hello/')
 def hello(name=None):
