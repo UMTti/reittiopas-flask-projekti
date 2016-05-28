c8a68ec7a1d2d5d561f7f1248a81a667a300546d
Henna Kalliokoski <henna.kalliokoski@ssh.com>
2016-05-28 14:51:44 +0300

    psycopg

--- run.py
+++ run.py
@@ -1,12 +1,22 @@
 
-from flask import Flask, url_for
-from flask import render_template
+from flask import Flask, url_for, render_template
+import os
+import psycopg2
+
+
 app = Flask(__name__)
 
+class Model:
+
+    def __init__(self, dbname, port):
+        self.conn = psycopg2.connect("dbname=%s port=%d" % (dbname, port))
+        self.cur = self.conn.cursor()
+        psycopg2.extensions.register_type(psycopg2.extensions.UNICODE, self.cur)
 
 @app.route('/hello/')
 def hello(name=None):
     return render_template('hello.html')
+
 
 @app.route('/sivut/')
 def default_page():
