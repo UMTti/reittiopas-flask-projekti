bd9151c6290b29e075136b03f26773308d05d034
Tintti Rahikainen <tintti.rahikainen@gmail.com>
2016-05-28 18:20:31 +0300

    added proper beacon id

--- run.py
+++ run.py
@@ -35,7 +35,7 @@ def search_buses_by_stop_id(stop_id):
 
 @app.route('/stop/<beacon_id>')
 def send_beacon_id(beacon_id):
-    beacon_ids = {'2f2f': 'HSL:1362141'}
+    beacon_ids = {'2f234454-cf6d-4a0f-adf2-f4911ba9ffa6': 'HSL:1362141'}
     buses = ""
     if beacon_id in beacon_ids:
         buses = search_buses_by_stop_id(beacon_ids[beacon_id])
