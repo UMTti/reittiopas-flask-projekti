27bb04d8a2003e41f9a77be3e3927b9d58a908af
Henna Kalliokoski <henna.kalliokoski@ssh.com>
2016-05-28 13:58:03 +0300

    index template

--- run.py
+++ run.py
@@ -1,13 +1,17 @@
 
 from flask import Flask, url_for
 from flask import render_template
-from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 
 
 @app.route('/hello/')
 def hello(name=None):
     return render_template('hello.html')
+
+@app.route('/sivut/')
+def default_page():
+    return render_template('index_template.html')
+
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
