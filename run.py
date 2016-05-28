484adb40824b0f67d1857fbcf756ded3a9833253
Henna Kalliokoski <henna.kalliokoski@ssh.com>
2016-05-28 17:00:44 +0300

    PEP8-clean

--- run.py
+++ run.py
@@ -4,13 +4,13 @@ import psycopg2
 
 app = Flask(__name__)
 
+
 class Model:
 
     def __init__(self, dbname, port):
         self.conn = psycopg2.connect("dbname=%s port=%d" % (dbname, port))
         self.cur = self.conn.cursor()
         psycopg2.extensions.register_type(psycopg2.extensions.UNICODE, self.cur)
-
 
     def sql(self, query, args=()):
         self.cur.execute(query, args)
@@ -19,11 +19,11 @@ class Model:
     def commit(self):
         self.conn.commit()
 
-    
 
 @app.route('/hello/')
 def hello(name=None):
     return render_template('hello.html')
+
 
 def search_buses_by_stop_id(stop_id):
     return stop_id
@@ -31,12 +31,13 @@ def search_buses_by_stop_id(stop_id):
 
 @app.route('/stop/<beacon_id>')
 def send_beacon_id(beacon_id):
-    beacon_ids = {'2f2f':'HSL:1362141'}
+    beacon_ids = {'2f2f': 'HSL:1362141'}
     buses = ""
     if beacon_id in beacon_ids:
         buses = search_buses_by_stop_id(beacon_ids[beacon_id])
 
     return buses
+
 
 @app.route('/sivut/')
 def default_page():
