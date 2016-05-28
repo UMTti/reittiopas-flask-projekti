02e4b28e4e1c4e336d065e870ca8a5f6a0db04ad
UMTti <toivanenpihla@gmail.com>
2016-05-28 12:42:51 +0300

    5000

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,5 +7,6 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT'))
+    #port = int(os.environ.get('PORT', 5000))
+    port = 5000
     app.run(host='0.0.0.0', port = port)
